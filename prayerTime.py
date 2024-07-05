29824e81795ed2fb4f94deac3ffc85ff8909ff02
Badee Khalbouss <79918603+Badee-pro@users.noreply.github.com>
2024-07-05 15:13:20 -0400

    All

---
+++ prayerTime.py
@@ -0,0 +1,35 @@
+import requests
+import json
+
+def get_prayer_times(country, city):
+    url = "http://api.aladhan.com/v1/timingsByCity"
+    params = {
+        "country": country,
+        "city": city,
+    }
+
+    response = requests.get(url, params=params)
+
+    if response.status_code == 200:
+        data = response.json()
+        
+        timings = data['data']['timings']
+        required_prayers = {prayer: timings[prayer] for prayer in ['Fajr','Sunrise', 'Dhuhr', 'Asr', 'Maghrib', 'Isha']}
+        
+        result = {
+            "country": country,
+            "city": city,
+            "prayer_times": required_prayers
+        }
+
+        with open("prayer_times.json", "w") as file:
+            json.dump(result, file)
+        # print(required_prayers)
+        print("Prayer times have been written to prayer_times.json")
+    else:
+        print(f"Failed to retrieve data: {response.status_code}")
+
+country = input("Enter the country: ")
+city = input("Enter the city: ")
+
+get_prayer_times(country, city)
